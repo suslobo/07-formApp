a5c11a4de9df175eef69af46d4381651c7b49965
slobo <suslobo@hotmail.es>
2024-08-01 14:42:08 +0200

    Creación de barra de navegación componente side-menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    SideMenuComponent //lo exportamos porque lo vamos a usar fuera de su módulo
   ]
 })
 export class SharedModule { }
