d2ee9d8ed8c50c256e9f177728f3204534817cb1
slobo <suslobo@hotmail.es>
2024-08-01 14:05:29 +0200

    Creación del enrutado de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //definimos las rutas
+  {
+    path: 'reactive', //path '' (vacío)
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+  },
+  //ponemos el comodin
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
