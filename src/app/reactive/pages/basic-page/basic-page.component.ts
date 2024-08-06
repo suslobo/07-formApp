31736c6af6fffe1c97122853259f1e3b260379e1
slobo <suslobo@hotmail.es>
2024-08-06 10:58:50 +0200

    Cómo hacer formulario reactivo y Validators y enlazarlo con el Html

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+/*   public myForm: FormGroup = new FormGroup({
+    name: new FormControl('', []),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }) */
+ //ahora con FormBuilder
+public myForm: FormGroup = this.fb.group({
+  name: ['', [ Validators.required, Validators.minLength(3) ] ],
+  price: [0, [ Validators.required, Validators.min(0)] ],
+  inStorage:  [0, [ Validators.required, Validators.min(0)] ],
+})
+
+ constructor( private fb: FormBuilder) {}
+
+ onSave():void {
+  console.log(this.myForm.value);
+ }
 
 }
