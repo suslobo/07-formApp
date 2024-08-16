d0da6fb734b2bcb11ac2ce8b8e48a4ddb6026ed6
slobo <suslobo@hotmail.es>
2024-08-16 12:25:17 +0200

    Validaciones de Email y patrones

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,5 +1,8 @@
-  import { FormControl, ValidationErrors } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
 
+//para la validación del email
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
 export const cantBeStrider = ( control: FormControl ): ValidationErrors | null => {
   // trim() limpia la posicion delante y detrás
