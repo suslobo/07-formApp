78fea6686842162b8fc8f2c28b1e685c42865d49
slobo <suslobo@hotmail.es>
2024-08-16 11:34:58 +0200

    Formulario de registro y actualizaciones en el app.module para su buen funcionamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -22,4 +23,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AuthRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
+// import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    // AppRoutingModule,
+    SharedModule,
+    CommonModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
