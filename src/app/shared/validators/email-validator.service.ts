007176be3638a67008e0a8a66712a4b65775b2a6
slobo <suslobo@hotmail.es>
2024-08-21 14:54:40 +0200

    Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -11,12 +11,37 @@ export class EmailValidator implements AsyncValidator {
     const email = control.value;
     console.log({ email })
 
+    const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+
+      console.log ({ email });
+
+      if ( email === 'fernando@google.com' )
+        subscriber.next({emailTaken: true});
+        subscriber.complete();
+
+      subscriber.next(null);
+      subscriber.complete();
+
+    }).pipe(
+      delay(3000)
+    );
+
+    return httpCallObservable;
+
+  }
+
+
+  /* validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+    const email = control.value;
+    console.log({ email })
+
     return of({
       emailTaken: true
     }).pipe(
       delay(2000)
     );
 
-  }
+  } */
 
 }
