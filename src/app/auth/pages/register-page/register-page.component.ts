2cb0fa674d9d80ca099f1577bbd73fb34457347a
slobo <suslobo@hotmail.es>
2024-08-21 14:21:54 +0200

    Validadores asincronos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,7 +11,7 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
-    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ], [ this.emailValidator] ],
     username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
@@ -20,6 +21,7 @@ export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
     private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
   ) {}
 
   isValidField( field: string) {
